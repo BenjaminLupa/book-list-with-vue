f3790079b33b7b8d04fb0c81f7acc9fc94591dde
Benjamin Lupa <147424978+BenjaminLupa@users.noreply.github.com>
2024-01-28 18:13:14 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <LayoutHeader />
+  <BookList />
+</template>
+
+<script>
+import LayoutHeader from "@/components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
+export default {
+  components: {
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
+
+<style></style>
